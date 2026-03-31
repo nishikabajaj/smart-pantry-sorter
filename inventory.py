c61516d59b7d13cc4c5a2bd22a2d5f4c256ecd0c
Nishika Bajaj <nb101604@gmail.com>
2026-03-30 23:02:37 -0500

    Set up localhost connection to DB

--- inventory.py
+++ inventory.py
@@ -1,1 +1,18 @@
 # Inventory management system for the smart pantry sorter project.
+
+import pyodbc
+
+# print(pyodbc.drivers()) # Confirm that the Microsoft ODBC Driver for SQL Server is installed.
+
+# connection string for Python -> DB
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=localhost\\SQLEXPRESS;'
+    'DATABASE=FoodPantryDB;'
+    'Trusted_Connection=yes;'
+)
+
+cursor = conn.cursor()
+cursor.execute('SELECT @@VERSION')
+print(cursor.fetchone())
+conn.close()
